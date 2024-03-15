6df73db2a1933226eb72d4646ae57321bb31fd8d
MellyKurokawa <melissa.gallegos39@uabc.edu.mx>
2024-03-15 15:56:17 -0700

    Practica 17_Gallegos Melissa

---
+++ Practica17/Practica17.js
@@ -0,0 +1,12 @@
+var num1;
+var num2;
+var suma, resta, multiplicacion, division;
+
+num1 = prompt ("Ingrese el primer numero:");
+num2 = prompt ("Ingrese el segundo numero");
+suma = (parseInt(num1) + parseInt(num2));
+resta = (parseInt(num1) - parseInt(num2));
+multiplicacion = (parseInt(num1) * parseInt(num2));
+division =(parseInt(num1) / parseInt(num2));
+
+alert("El resultado de la suma : "+ suma +" \nEl resultado de la resta es:  "+ resta +" \nEl resultado de la multiplicacion es: "+ multiplicacion +"\nEl resultado de la division es: "+ division);
