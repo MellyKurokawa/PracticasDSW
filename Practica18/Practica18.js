f9095ac7d1fc8779f999c9a703df02f7eca3311e
MellyKurokawa <melissa.gallegos39@uabc.edu.mx>
2024-03-15 18:21:16 -0700

    Practica 18_Gallegos Melissa

---
+++ Practica18/Practica18.js
@@ -0,0 +1,17 @@
+//Melissa Fernanda Gallegos Rojas
+
+//Creamos el array vacío para almacenar los días de la semana
+let diasSemana = [];
+
+//Añade los días de la semana al array
+//Con el push agregamos cada día de la semana al array
+diasSemana.push('Lunes');
+diasSemana.push('Martes');
+diasSemana.push('Miércoles');
+diasSemana.push('Jueves');
+diasSemana.push('Viernes');
+diasSemana.push('Sábado');
+diasSemana.push('Domingo');
+
+//Despliega los días de la semana en el alert
+alert(diasSemana.join(', '));
